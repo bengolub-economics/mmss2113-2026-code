8ec4846ca4fd64f6fcc6731813bd9de01f873316
Ben Golub <ben.golub@gmail.com>
2026-04-17 13:52:33 -0500

    Wire form to Apps Script web-app URL

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 // Paste the Apps Script Web App URL here after deploying.
 // Example: "https://script.google.com/macros/s/AKfycbx.../exec"
 window.APP_CONFIG = {
-  scriptUrl: "PASTE_APPS_SCRIPT_WEB_APP_URL_HERE"
+  scriptUrl: "https://script.google.com/macros/s/AKfycbz9XGpJpwcgmrtD_6K9aV5EPtx2rMzu5TjQSyXHUZbpghCk_JcNa2gimayKLYLi4JP-hw/exec"
 };
